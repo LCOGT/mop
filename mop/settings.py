3ca5f534e1bd71a0519bb5cfdda3910e62dff5f5
T. Andrew Manning <manninga@illinois.edu>
2022-04-11 09:36:01 -0500

    Support arbitrary S3 bucket URL. Allow S3 bucket config from Secret.

--- mop/settings.py
+++ mop/settings.py
@@ -189,6 +189,7 @@ MEDIA_URL = '/data/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL', '')
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY', '')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_S3_BUCKET', '')
