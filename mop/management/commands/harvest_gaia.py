4108f91b3d7a243981f8e40cec802ae7b17f62ee
rachel3834 <rstreet@lcogt.net>
2024-05-31 17:27:10 -0700

    Updated gaia_harvester to custom Target model

--- mop/management/commands/harvest_gaia.py
+++ mop/management/commands/harvest_gaia.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from microlensing_targets.models import MicrolensingTarget
 from tom_alerts.brokers import gaia
 from tom_targets.models import Target
 from mop.brokers import gaia as gaia_mop
@@ -69,7 +70,7 @@ class MOPGaia(gaia.GaiaBroker):
                                 source_location=alert_url,
                                 data_type='photometry',
                                 target=target)
-                        
+
 
             (t_last_jd, t_last_date) = TAP.TAP_time_last_datapoint(target)
             extras = {'Latest_data_HJD': t_last_jd, 'Latest_data_UTC': t_last_date}
@@ -110,10 +111,16 @@ class Command(BaseCommand):
             #Create or load
             clean_alert = Gaia.to_generic_alert(alert)
             try:
-               target, created = Target.objects.get_or_create(name=clean_alert.name,ra=clean_alert.ra,dec=clean_alert.dec,type='SIDEREAL',epoch=2000)
+               target, created = MicrolensingTarget.objects.get_or_create(
+                   name=clean_alert.name,
+                   ra=clean_alert.ra,
+                   dec=clean_alert.dec,
+                   type='SIDEREAL',
+                   epoch=2000
+               )
             #seems to bug with the ra,dec if exists
             except:
-                  target, created = Target.objects.get_or_create(name=clean_alert.name)
+                  target, created = MicrolensingTarget.objects.get_or_create(name=clean_alert.name)
 
             if created:
                 new_alerts.append(target)
