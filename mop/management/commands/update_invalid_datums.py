3526ff67d80fb720b8d98fb9e45ba281822ad6cb
rachel3834 <rstreet@lcogt.net>
2024-06-21 13:28:00 -0700

    Added tool to fix missing source name and location parameters for some uploaded ReducedDatums

---
+++ mop/management/commands/update_invalid_datums.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from tom_targets.models import Target
+from tom_dataproducts.models import ReducedDatum, DataProduct
+
+class Command(BaseCommand):
+
+    help = "Command to review all ReducedDatums and identify those with invalid source IDs"
+
+    def handle(self, *args, **options):
+
+        data_list = ReducedDatum.objects.filter(source_name='Gaia')
+        print('Found ' + str(data_list.count()) + ' ReducedDatums to review')
+
+        for rd in data_list:
+
+            if len(rd.source_name) == 0 or rd.source_name == 'None' or rd.source_name == None:
+
+                print(rd.pk, rd.source_name, rd.source_location, rd.data_product)
+                # Try to identify the origin of the data if possible
+                if 'auto.csv' in rd.data_product.data:
+                    rd.source_name = 'OMEGA'
+                    rd.source_location = 'MOP'
+                    rd.save()
+                    print('Updated ReducedDatum ' + str(rd.pk))
