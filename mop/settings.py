8dc5a846c74b3edd476c9802ef96e34951542f45
rachel3834 <rstreet@lcogt.net>
2024-07-10 16:40:33 -0700

    Set strict_json

--- mop/settings.py
+++ mop/settings.py
@@ -127,7 +127,8 @@ REST_FRAMEWORK = {
     ],
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'STRICT_JSON': False,
 }
 
 
