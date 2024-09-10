afcfd8e7c67d6fee2f9322bed2d4fbb0be5466db
rachel3834 <rstreet@lcogt.net>
2024-09-10 14:38:17 -0700

    Generalized Gaia search tool

--- mop/management/commands/fetch_gaia_data_for_event.py
+++ mop/management/commands/fetch_gaia_data_for_event.py
@@ -12,21 +12,27 @@ class Command(BaseCommand):
 
     help = 'Retrieves Gaia DR3 data for a given event'
     def add_arguments(self, parser):
-        parser.add_argument('event', help='name of a specific event, all or an integer number')
+        parser.add_argument('event', help='name of a specific event, or ALL')
 
     def handle(self, *args, **options):
         logger.info('Starting run of Gaia DR3 data retrieval')
 
-        qs = Target.objects.filter(name=options['event'])
+        if 'ALL' in options['event']:
+            target_list = Target.objects.all()
+        else:
+            qs = Target.objects.filter(name=options['event'])
+            if qs.count() > 0:
+                target_list = [qs[0]]
 
-        if qs.count() > 0:
-            target = qs[0]
-            gaia_mop.fetch_gaia_dr3_entry(target, radius=Angle(0.00014, "deg"))
+        if len(target_list) > 0:
+            for j,target in enumerate(target_list):
+                gaia_mop.fetch_gaia_dr3_entry(target, radius=Angle(0.00014, "deg"))
 
-            # This alternative async TAP query is EXTREMELY slow
-            #gaia_mop.query_gaia_tap_service(target, radius=Angle(0.00014, "deg"), row_limit=-1)
+                # This alternative async TAP query is EXTREMELY slow
+                #gaia_mop.query_gaia_tap_service(target, radius=Angle(0.00014, "deg"), row_limit=-1)
 
-            logger.info('Completed fetch of Gaia DR3 data for ' + options['event'])
+                logger.info('Completed fetch of Gaia DR3 data for ' + target.name
+                            + ', ' + str(j) + ' of ' + str(len(target_list)))
 
         else:
             logger.info('No targets matching the name ' + options['event'] + ' found')
