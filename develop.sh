5ed8d6e0681556fa3d7675bd02b829aa4f992e57
Jashandeep Sohi <jsohi@lco.global>
2025-02-12 16:38:41 -0800

    accept flake config

--- develop.sh
+++ develop.sh
@@ -3,4 +3,4 @@
 DEVENV_ROOT_FILE="$(mktemp)"
 printf %s "$PWD" > "$DEVENV_ROOT_FILE"
 
-nix develop --override-input devenv-root "file+file://$DEVENV_ROOT_FILE"
+nix develop --accept-flake-config --override-input devenv-root "file+file://$DEVENV_ROOT_FILE"
