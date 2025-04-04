cb0868b95b07e38c2bd8d669e1f592ea47211956
KKruszynska <kruszynskakat@gmail.com>
2025-04-04 15:02:47 -0700

    debugging

--- mop/toolbox/interferometry_prediction.py
+++ mop/toolbox/interferometry_prediction.py
@@ -282,8 +282,10 @@ def search_gsc_catalog(target):
     logger.info('INTERFERO: Querying GSC catalog around event ' + target.name)
 
     # Perform Vizier catalog search of the GSC
+    print('Target: ', target)
+    print('Target type', type(target))
     gsc_table = gsc.query_gsc(target)
-    logger.info('GSC Table: \n' + gsc_table)
+    print('GSC Table: \n' + gsc_table)
 
     # If any stars are missing Ks-band magnitudes, attempt to estimate them using calibrations
     # based on other photometric bands
