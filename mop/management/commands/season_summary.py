60e933ef14c4467d7dde6615d882862b71b4dec9
rachel3834 <rstreet@lco.global>
2026-04-11 14:49:56 -0700

    Added tool to create post-season observation summary

---
+++ mop/management/commands/season_summary.py
@@ -0,0 +1,49 @@
+from django.core.management.base import BaseCommand
+from tom_dataproducts.models import ReducedDatum
+from tom_observations.models import ObservationRecord
+from tom_targets.models import Target, TargetName, TargetList
+from django.db.models import Q
+from datetime import datetime, timezone
+
+class Command(BaseCommand):
+
+    help = 'Summarize targets and data acquired over a given interval'
+    def add_arguments(self, parser):
+        parser.add_argument('start_date', help='Start date of time range in YYYY-MM-DD format')
+        parser.add_argument('end_date', help='End date of time range in YYYY-MM-DD format')
+
+    def handle(self, *args, **options):
+
+        # Parse the time range given
+        start_date = datetime.strptime(options['start_date'], "%Y-%m-%d").replace(tzinfo=timezone.utc)
+        end_date = datetime.strptime(options['end_date'], "%Y-%m-%d").replace(tzinfo=timezone.utc)
+
+        # Identify a queryset of events with ObservationRecords with timestamps between
+        # the given start and end dates.  Scheduled start and end timestamps are not always
+        # populated since this is only done on update from the scheduler, so we have to
+        # extract this from the parameters
+        targets = []
+        qs = ObservationRecord.objects.all()
+        for obs in qs:
+            obs_start = datetime.fromisoformat(obs.parameters['start'])
+            if obs_start >= start_date and obs_start <= end_date:
+                if obs.target not in targets: targets.append(Target.objects.get(id=obs.target.pk))
+        print('Found ' + str(len(targets)) + ' targets with observation records')
+
+        # For those targets, review the ReducedDatums obtained
+        datums = ReducedDatum.objects.filter(target__in=targets).order_by("timestamp")
+        event_data = {x: {} for x in targets}
+        nomega = 0
+        min_obs = 10 # Threshold for meaningful observations
+        for mulens in event_data.keys():
+            mulens.get_reduced_data(datums.filter(target=mulens))
+            event_data[mulens] = {key: len(arr) for key,arr in mulens.datasets.items()}
+
+            print(mulens.name + ' : '
+                  + ' '.join([key + '=' + str(count) for key,count in event_data[mulens].items()]))
+
+            # Check what data OMEGA obtained
+            for key in mulens.datasets.keys():
+                if 'OMEGA' in key and len(mulens.dataset[key]) >= min_obs:
+                    nomega += 1
+        print('OMEGA observations of ' + str(nomega) + ' targets')
