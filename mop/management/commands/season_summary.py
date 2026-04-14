165f4f76083bf7fa377b3cd1db8fb46b3ff9709f
rachel3834 <rstreet@lco.global>
2026-04-14 13:24:06 -0700

    Remove debug statements

--- mop/management/commands/season_summary.py
+++ mop/management/commands/season_summary.py
@@ -29,14 +29,11 @@ class Command(BaseCommand):
                 obs_start = datetime.fromisoformat(obs.parameters['start']).replace(tzinfo=timezone.utc)
 
             except KeyError:
-                print(obs.parameters['requests'])
                 obs_ts = obs.parameters['requests'][0]['windows'][0]['start']
-                print(obs_ts)
                 obs_start = datetime.strptime(
-                    obs.parameters['requests'][0]['windows'][0]['start'],
+                    obs_ts,
                     "%Y-%m-%d %H:%M:%S"
                 ).replace(tzinfo=timezone.utc)
-                breakpoint()
 
             if obs_start >= start_date and obs_start <= end_date:
                 if obs.target not in targets: targets.append(Target.objects.get(id=obs.target.pk))
